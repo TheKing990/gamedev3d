fd138f87cbe5f6683876c768cf7f3a64fc49b84a
Jhovany Gonzalez <jhovanygonzalez@Jhovanys-MacBook-Pro-2.local>
2017-04-03 19:38:17 -0500

    adding assets

---
+++ Assets/Assets/CameraPov.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPov : MonoBehaviour {
+
+
+
+	[Header("Atrributes")]
+	//------------upgrdadeable stuffs---------------
+
+	public float turnspeed = 7f;
+
+
+
+	[Header("Unity stuff")]
+	//----------------------------user no change---------
+	public string playerTag = "Player";
+	public Transform partToRotate;
+	private Transform target;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("UpdateTarget", 0f, .3f);
+		
+	}
+
+
+
+
+	void UpdateTarget()
+	{
+
+		GameObject[] players = GameObject.FindGameObjectsWithTag (playerTag);
+		float shortestDistance = Mathf.Infinity;
+		GameObject nearestEnemy = null;
+
+		foreach (GameObject player in players) 
+		{
+			float distanceToEnemy = Vector3.Distance (transform.position, player.transform.position);
+			if (distanceToEnemy < shortestDistance) 
+			{
+				shortestDistance = distanceToEnemy;
+				nearestEnemy =player;
+			}
+
+		}
+
+
+		if (nearestEnemy != null ) {
+			target = nearestEnemy.transform;
+		} else {
+			target = null;
+		}
+
+	}
+
+
+
+
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (target == null)
+			return;
+
+		//lockon
+		Vector3 dir = target.position - transform.position;
+		Quaternion lookRotation = Quaternion.LookRotation (dir);
+		Vector3 rotation = Quaternion.Lerp(partToRotate.rotation,lookRotation,Time.deltaTime*turnspeed).eulerAngles;
+		partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f);
+
+		
+	}
+
+
+
+
+
+}
---
+++ Assets/Assets/ClickMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class ClickMove : MonoBehaviour {
+    public Camera cam;
+    public NavMeshAgent navMeshAgent;
+
+
+	void Start ()
+	{
+        cam = Camera.main;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+	}
+
+	void Update(){
+        if (Input.GetKeyDown(KeyCode.Mouse0)){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+
+            if(Physics.Raycast(ray,out hit,100)) {
+                navMeshAgent.SetDestination(hit.point);
+            }
+
+        }
+	}
+}
